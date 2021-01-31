08430d8f7cb0b30729dd5e3702e87c411fa6fd62
Ackerman <929227881@qq.com>
2021-01-31 15:52:23 +0800

    PHP FILE

---
+++ PHP/index的副本.php
@@ -0,0 +1,52 @@
+<?php
+$servername = "172.17.0.2";
+$username = "ackerman";
+$password = "123456";
+$dbname = "ackerman";
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if ($conn->connect_error)
+{
+    die("连接失败: " . $conn->connect_error);
+}
+
+//$sql = "CREATE TABLE MyGuests (
+//    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//    firstname VARCHAR(30) NOT NULL,
+//    lastname VARCHAR(30) NOT NULL,
+//    email VARCHAR(50),
+//    reg_data TIMESTAMP
+//)";
+//
+//if ($conn->query($sql) === TRUE) {
+//    echo "Table MyGuests created successfully";
+//} else {
+//    echo "创建数据表错误: " . $conn->error;
+//}
+
+// 检测插入单条数据是否成功
+
+//$sql = "INSERT INTO MyGuests (firstname,lastname,email) VALUES ('John','Doe','john@example.com')";
+//
+//if ($conn->query($sql) == TRUE)
+//    echo "Succesful!";
+//else
+//    echo "Error: " . $sql . "<br>" . $conn->error;
+//
+
+// 检测插入多条数据是否成功
+
+//$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+//VALUES ('John', 'Doe', 'john@example.com');";
+//$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+//VALUES ('Mary', 'Moe', 'mary@example.com');";
+//$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+//VALUES ('Julie', 'Dooley', 'julie@example.com')";
+
+//if ($conn->multi_query($sql) == TRUE)
+//    echo "Succesful!";
+//else
+//    echo "Error: " . $sql . "<br>" . $conn->error;
+
+
+$conn->close();
